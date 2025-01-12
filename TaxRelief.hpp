1cb34ec21faf42ede57e2e3b82e90ed08f21587e
mahavigna <mahavigna@student.usm.my>
2025-01-13 03:51:59 +0800

    Reorganised files and folder. Other folders are references to use for logic. File commited now is for tax relief. Rebate will be done after tax calculation system is done

---
+++ TaxRelief.hpp
@@ -0,0 +1,14 @@
+#ifndef TAXRELIEF_HPP
+#define TAXRELIEF_HPP
+
+void Marital_Status(double& TotalTaxRelief1);
+void Children(double& TotalTaxRelief2);
+void Contribution(double& TotalTaxRelief3);
+void Lifestyle(double& TotalTaxRelief4);
+double Total_Tax_Relief(double TotalTaxRelief1, double TotalTaxRelief2, double TotalTaxRelief3, double TotalTaxRelief4);
+
+void PrintReliefs();
+double childcarefee = 0.0; // Initialize the variable childcarefee
+
+
+#endif
